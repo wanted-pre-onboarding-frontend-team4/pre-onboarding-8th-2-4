8a2f21e424a56e0bf9d1322dd8e0751d89aa38b3
hyejineee <dev.hyejin04@gmail.com>
2023-01-04 13:21:44 +0900

    settings : recoil 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { RecoilRoot } from 'recoil'
 import GlobalStyles from './commons/styles/globalStyles'
 import BoardContainer from './components/board.container'
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyles />
       <BoardContainer />
-    </>
+    </RecoilRoot>
   )
 }
 
